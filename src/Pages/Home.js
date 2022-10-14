1b852f20b6920b01656bea715ea365919ab1ebf1
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-14 23:05:54 +0530

    update login page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,71 +2,52 @@ import React from 'react';
 import "../CSS/Home.css";
 import GoogleLogo from "../Assets/icons8-google-48.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import logo from '../Assets/Group 3.png'
 
 function Home() {
   return (
-    
-    <div >
-       <div className='split left'>
-        <div className='text-left'>
-           <div className="title">Get Repo </div>
-           <div className="tagline">Ease your searching Journey with us.</div>
-           <p className="text1">GetRepos helps you to find your github repository at just the tip of your finger with various search filters.</p>
-           <div className='content '></div>
- 
-
-           <div class="slidershow middle">
-
-            <div class="slides">
-              {/* <input type="radio" name="r" id="r1" checked> */}
-              {/* <input type="radio" name="r" id="r2">
-              <input type="radio" name="r" id="r3">
-              <input type="radio" name="r" id="r4">
-              <input type="radio" name="r" id="r5"> */}
-              <div class="slide s1">
-                {/* <img src="1.jpg" alt=""> */}
-                <div className='content '><h1>hii</h1></div>
+    <div className='home-container'>
+      <div className='left-container'>
+        <h1 className='logo'>GetRepo</h1>
+        <div className='left-container_contents'>
+          <p>Ease your searching Journey with us.</p>
+          <p>GetRepo helps you to find your github repository at just the tip of your finger with various search filters.</p>
+        </div>
+        <div className='left-container_testimonial-container'>
+           <div className='left-container_testimonial'>
+           
+           </div>
+           <div className='left-container_testimonial-btns'>
+              <div className='left-container_testimonial-btn active'></div>
+              <div className='left-container_testimonial-btn faded'></div>
+              <div className='left-container_testimonial-btn faded'></div>
+              <div className='left-container_testimonial-btn faded'></div>
               </div>
-              <div class="slide">
-                {/* <img src="2.jpg" alt=""> */}
-                <div style={{backgroundColor: "lightblue"}} className='content '></div>
-              </div>
-              <div class="slide">
-              <div className='content '></div>
-              </div>
-              <div class="slide">
-              <div className='content '></div>
-              </div>
-              <div class="slide">
-              <div className='content '></div>
-              </div>
-            </div>
-
-            <div class="navigation">
-              <label for="r1" class="bar"></label>
-              <label for="r2" class="bar"></label>
-              <label for="r3" class="bar"></label>
-              <label for="r4" class="bar"></label>
-              <label for="r5" class="bar"></label>
-            </div>
-          </div>
-
-        </div> 
-          
+        </div>
+      </div>
+      <div className='right-container'>
+      <h1 className='right-container_heading'>Login</h1>
+      <div className='right-container_contents'>
+        <p className='right-container_info'>
+        Login securely , with world’s most safest authentication
+        </p>
+        <div className='right-container_btn'>
+          <img src={logo} alt="" />
+          <p>Continue With Google </p>
+        </div>
       </div>
 
-      <div className='split right'>
-        <div className='login'>Login</div>
-        <div className='loginText'>Login securely, with world's most safest authentication..</div>
-        <div className="googleLoginTextBox"></div>
-        <div className='googleLogin'>Continue with Google</div>
-        <img className='logoGoogle' src={GoogleLogo} alt="Logo" />
-        <div className="developed">Developed by</div>
-        <div className='developer1'>Rudransh Khandelwal</div>
-        <div className='developer2'>Hitesh Tahiliani</div>
+      <div className='right-container_devlopers'>
+        <div className='developers-heading'><p>Devloped by</p> </div>
+        <div className='developer'>
+          <p>Rudransh Khandelwal</p>
+          <p>Hitesh Telhani</p>
+        </div>
+      </div>
+
       </div>
     </div>
+  
   )
 }
 
