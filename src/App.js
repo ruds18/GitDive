317728e48d9ef21f034dc3130dae72618706835d
Hitesh <hitesh11tahiliani@gmail.com>
2022-10-14 18:05:50 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "../src/Pages/Home";
 
 function App() {
   return (
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "../CSS/Home.css";
+import GoogleLogo from "../Assets/icons8-google-48.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Home() {
   return (
@@ -7,14 +10,59 @@ function Home() {
     <div >
        <div className='split left'>
         <div className='text-left'>
-           <h1 className="title">Get Repo </h1>
-           <div className="tagline">Ease your searching <br></br>Journey with us.</div>
-           <p className="text1">GetRepos helps you to find your github repository at <br></br></p>
+           <div className="title">Get Repo </div>
+           <div className="tagline">Ease your searching Journey with us.</div>
+           <p className="text1">GetRepos helps you to find your github repository at just the tip of your finger with various search filters.</p>
+           <div className='content '></div>
+
+           <div>
+            
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                </div>
+                <div class="carousel-item">
+                  <div className='content '>1</div>
+                  {/* <img class="d-block w-100" src="..." alt="Second slide"> */}
+                </div>
+                <div class="carousel-item">
+                  <div className='content '>2</div>
+                  {/* <img class="d-block w-100" src="..." alt="Third slide"> */}
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+            
+           </div>
+           
+
+            
+
         </div> 
           
       </div>
 
       <div className='split right'>
+        <div className='login'>Login</div>
+        <div className='loginText'>Login securely, with world's most safest authentication..</div>
+        <div className="googleLoginTextBox"></div>
+        <div className='googleLogin'>Continue with Google</div>
+        <img className='logoGoogle' src={GoogleLogo} alt="Logo" />
+        <div className="developed">Developed by</div>
+        <div className='developer1'>Rudransh Khandelwal</div>
+        <div className='developer2'>Hitesh Tahiliani</div>
       </div>
     </div>
   )
